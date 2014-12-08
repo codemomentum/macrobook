227584b7922e8386c85557c8a8366eb2a2fad1d1
Halit Olali <halitolali@gmail.com>
2014-12-08 22:11:27 +0100

    macro book examples

--- src/macrobook/core.clj
+++ src/macrobook/core.clj
@@ -1,6 +1,40 @@
-(ns macrobook.core)
+(ns macrobook.core
+  (:require [clojure.pprint :as pp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord RemoteMapper [content])
+
+(defmacro defmapper
+  "runs the function f over the content
+
+  f is a function of two arguments, key and value.
+
+  f must return a sequence of *pairs* like
+   [[key1 value1] [key2 value2] ...]
+  "
+  [fname args & body]
+  `(let [instance# (RemoteMapper.
+                     (pr-str '(fn ~args ~@body)))]
+     (def ~fname instance#)))
+
+(defmapper mapper1 [x] (* x 2))
+
+mapper1
+
+(gensym "xyz")
+
+(defmacro info-about-caller []
+  (pp/pprint {:form &form :env &env})
+  `(println "macro was called!"))
+
+(info-about-caller)
+
+(let [x 5] (info-about-caller))
+
+
+(defmacro inspect-caller-locals [] (->> (keys &env)
+                                        (map (fn [k] [`'~k k])) (into {})))
+
+(let [x 5] (inspect-caller-locals))
+
+
+
